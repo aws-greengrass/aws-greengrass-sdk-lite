5ea9f0aa6d91d0f48a8d3c37d5429d7e26a7cdb0
Patrick Cook <cookpate@amazon.com>
2025-12-18 15:48:31 -0800

    Move object comparisons from mock to gg-test

--- unity/gg_test/include/gg/test.h
+++ unity/gg_test/include/gg/test.h
@@ -1,6 +1,14 @@
 #ifndef GG_TEST_UNITY_HELPERS_H
 #define GG_TEST_UNITY_HELPERS_H
 
+#ifdef __cplusplus
+#include <gg/types.hpp>
+#else
+#include <gg/buffer.h>
+#include <gg/object.h>
+#include <stdbool.h>
+#endif
+#include <sys/types.h>
 #include <unity.h>
 #include <stddef.h>
 
@@ -48,25 +56,82 @@ int gg_test_run_suite(void);
 #define GG_TEST_ASSERT_OK(expr) TEST_ASSERT_EQUAL(GG_ERR_OK, (expr))
 #define GG_TEST_ASSERT_BAD(expr) TEST_ASSERT_NOT_EQUAL(GG_ERR_OK, (expr))
 
+void gg_test_assert_obj_equal(
+    GgObject expected, GgObject actual, const char *message, UNITY_UINT line_no
+);
+
+void gg_test_assert_buf_equal(
+    GgBuffer expected, GgBuffer actual, const char *message, UNITY_UINT line_no
+);
+
+void gg_test_assert_buf_equal_str(
+    GgBuffer expected, GgBuffer actual, const char *message, UNITY_UINT line_no
+);
+
+void gg_test_assert_kv_equal(
+    GgKV expected, GgKV actual, const char *message, UNITY_UINT line_no
+);
+
+void gg_test_assert_map_equal(
+    GgMap expected, GgMap actual, const char *message, UNITY_UINT line_no
+);
+
+void gg_test_assert_list_equal(
+    GgList expected, GgList actual, const char *message, UNITY_UINT line_no
+);
+
+static inline void gg_test_assert_int64_equal(
+    int64_t expected, int64_t actual, const char *message, UNITY_UINT line_no
+) {
+    UnityAssertEqualNumber(
+        (UNITY_INT) expected,
+        (UNITY_INT) actual,
+        message,
+        line_no,
+        UNITY_DISPLAY_STYLE_INT64
+    );
+}
+
+static inline void gg_test_assert_bool_equal(
+    bool expected, bool actual, const char *message, UNITY_UINT line_no
+) {
+    UnityAssertEqualNumber(
+        expected, actual, message, line_no, UNITY_DISPLAY_STYLE_INT
+    );
+}
+
+static inline void gg_test_assert_f64_equal(
+    double expected, double actual, const char *message, UNITY_UINT line_no
+) {
+    UnityAssertDoublesWithin(0.001, expected, actual, message, line_no);
+}
+
+#define GG_TEST_ASSERT_OBJ_EQUAL(expected, actual) \
+    gg_test_assert_obj_equal((expected), (actual), NULL, __LINE__)
+
+#define GG_TEST_ASSERT_I64_EQUAL(expected, actual) \
+    gg_test_assert_i64_equal((expected), (actual), NULL, __LINE__)
+
+#define GG_TEST_ASSERT_BOOL_EQUAL(expected, actual) \
+    gg_test_assert_bool_equal((expected), (actual), NULL, __LINE__)
+
+#define GG_TEST_ASSERT_F64_EQUAL(expected, actual) \
+    gg_test_assert_f64_equal((expected), (actual), NULL, __LINE__)
+
 #define GG_TEST_ASSERT_BUF_EQUAL(expected, actual) \
-    do { \
-        TEST_ASSERT_EQUAL_size_t_MESSAGE( \
-            (expected).len, (actual).len, "Size mismatch" \
-        ); \
-        TEST_ASSERT_EQUAL_CHAR_ARRAY( \
-            (char *) (expected).data, (char *) (actual).data, (expected).len \
-        ); \
-    } while (0)
+    gg_test_assert_buf_equal((expected), (actual), NULL, __LINE__)
 
 #define GG_TEST_ASSERT_BUF_EQUAL_STR(expected, actual) \
-    do { \
-        TEST_ASSERT_EQUAL_size_t_MESSAGE( \
-            (expected).len, (actual).len, "Size mismatch" \
-        ); \
-        TEST_ASSERT_EQUAL_STRING_LEN( \
-            (expected).data, (actual).data, (expected).len \
-        ); \
-    } while (0)
+    gg_test_assert_buf_equal_str((expected), (actual), NULL, __LINE__)
+
+#define GG_TEST_ASSERT_MAP_EQUAL(expected, actual) \
+    gg_test_assert_map_equal((expected), (actual), NULL, __LINE__)
+
+#define GG_TEST_ASSERT_KV_EQUAL(expected, actual) \
+    gg_test_assert_kv_equal((expected), (actual), NULL, __LINE__)
+
+#define GG_TEST_ASSERT_LIST_EQUAL(expected, actual) \
+    gg_test_assert_list_equal((expected), (actual), NULL, __LINE__)
 
 #ifdef ENABLE_COVERAGE
 // NOLINTNEXTLINE(readability-identifier-naming)
